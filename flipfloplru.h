fa2542d721356523e2e7447a34fd0aaf53008d5a
T Percival <ted@tedp.id.au>
2020-11-19 01:01:50 -0800

    Here's a thing, I guess.

--- flipfloplru.h
+++ flipfloplru.h
@@ -1,11 +1,54 @@
-#include <vector>
+#include <unordered_map>
 
-template <typename T>
+template <typename Key, typename Value>
 class FlipFlopLRU {
-    T* Get();
-    void Put(T&& element);
+public:
+    Value* Get(const Key& key);
+    void Put(const K& key, V&& element);
 
 private:
-    std::vector<T> left_, right_;
-    std::vector<T> *active_ = &left_, *passive_ = &right_;
+    void Swap();
+
+    std::unordered_map<Key, Value> left_, right_;
+    std::unordered_map<Key, Value> *active_ = &left_, *passive_ = &right_;
+
+    size_t max_size_ = 0;
 };
+
+
+template <typename Key, typename Value>
+Value* FlipFlopLRU<key, Value>::Get(const Key& key) {
+    auto found = active_->find(key);
+    if (found != active_->end()) {
+        return found->second;
+    }
+
+    found = passive_->find(key);
+    if (found == passive_->end()) {
+        return nullptr_;
+    }
+    else {
+        found = active_->insert(key, move(found->second));
+        return &found->second;
+    }
+
+
+    return nullptr;
+}
+
+template <typename Key, typename Value>
+void FlipFlopLRU<Key, Value>::Put(const Key& key, Value&& value) {
+    if (max_size_ == active->size())
+    {
+        Swap();
+        passive_->clear();
+    }
+
+    active_->emplace(key, move(value));
+}
+
+template <typename Key, typename Value>
+void FlipFlopLRU<key, Value>::Swap() {
+    std::swap(active_, passive_);
+    passive.clear();
+}
--- test.cpp
+++ test.cpp
@@ -1,14 +1,21 @@
 #include "flipfloplru.h"
 
 struct Movable {
+public:
     Movable() = default;
+    explicit Movable(int v): v_(v) {}
     Movable(const Movable&) = delete;
     Movable(Movable&&) = default;
     Movable& operator=(const Movable&) = delete;
     Movable& operator=(Movable&&) = default;
+private:
+    int v_ = 0;
 };
 
 int main(void) {
     FlipFlopLRU<Movable> lru;
+
+    lru.Put(Movable(1));
+
     return 0;
 }
